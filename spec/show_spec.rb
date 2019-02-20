51b0e27aa844e376bb11d0e49681215cc38c25ce
hansenjl <jenn.leigh.hansen@gmail.com>
2019-02-20 14:51:03 -0800

    finish tests for show class

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -19,35 +19,59 @@ describe "Show" do
       expect(Show.all).to include(friends)
     end
 
-     it "is initialized with an empty array to store all of it's episodes" do
+  end
+
+
+  describe '#episodes' do
+    it "is initialized with an empty array to store all of it's episodes" do
       expect(friends.episodes).to eq([])
     end
 
-  end
+    it "has many episodes" do
+      episode = Episode.new("The One With Rachel's Sister")
+      friends.episodes << episode
 
-  # describe "#num_of_episodes" do
-  #     it "can have a num_of_episodes" do
-  #       friends.num_of_episodes = 2
-  #       expect(friends.num_of_episodes).to eq("10-9-2003")
-  #     end
-  # end
-
-  describe '#episodes' do
-    it "can belong to a show" do
-      friends = Show.new("Friends")
-      ross_tan.show = friends
-      expect(ross_tan.show).to be_a(Show)
+      expect(friends.episodes).to include(episode)
     end
   end
 
-   describe '#show=' do
-    #You will need to complete the Show tests before tackling this one
-    it "will also add the episode to the show's array of episodes" do
-      the_office = Show.new("The Office")
-      stairmaggedon = Episode.new("Stairmaggedon")
-      stairmaggedon.show = the_office
-      expect(the_office.episodes).to include(stairmaggedon)
+  describe '#add_episode' do
+    it "it accepts an episode as an argument and adds it to the episodes array " do
+      episode = Episode.new("The One With the Cop")
+      friends.add_episodes(episode)
+      expect(friends.episodes).to include(episode)
+    end
+
+    it "assigns the episode's show to be itself" do
+      episode = Episode.new("The One With the Cop")
+      friends.add_episodes(episode)
+      expect(episode.show).to eq(friends)
+    end
+
+    it "doesn't add the same episode twice" do
+      friends.episodes = []
+      episode = Episode.new("The One With the Cop")
+      other_episode = Episode.new("The One With the Yeti")
+      friends.add_episodes(episode)
+      friends.add_episodes(episode)
+      friends.add_episodes(other_episode)
+      expect(friends.episodes.length).to eq(2)
     end
   end
+
+    describe "#num_of_episodes" do
+      it "returns the number of episodes added to the #episodes array" do
+        friends.episodes = []
+        episode = Episode.new("The One With the Cop")
+        other_episode = Episode.new("The One With the Yeti")
+        another_episode = Episode.new("The One Where Ross Moves In")
+        friends.add_episodes(episode)
+        friends.add_episodes(other_episode)
+        friends.add_episodes(another_episode)
+        expect(friends.num_of_episodes).to eq(3)
+      end
+  end
+
+
 
 end
